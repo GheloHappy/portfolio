7b7719a2210c229fa66dff03866dd17e5fd4ff47
Ghelonico <ghelonicomaligaya111@gmail.com>
2023-10-09 17:24:22 +0800

    about page update

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,6 +1,7 @@
 import '../css/about.css';
 import propic from '../assets/img/propic_v4.png';
 import { useEffect, useState } from 'react';
+import Summary from './about/Summary.jsx'
 
 const calculateAge = (birthday, experience) => {
     const currentDate = new Date();
@@ -14,7 +15,7 @@ const calculateAge = (birthday, experience) => {
     const expDate = new Date(expDiff);
     const exp = Math.abs(expDate.getUTCFullYear() - 1970);
 
-    return [age,exp];
+    return [age, exp];
 };
 
 const About = () => {
@@ -23,37 +24,67 @@ const About = () => {
     const [birthday, setBirthday] = useState(defaultBirthday);
     const [experience, setExperience] = useState(defaultExpDate)
     const [age, exp] = calculateAge(birthday, experience);
+    const [cardType, setCardType] = useState("")
+
+
+    const paragraphItems = [
+        {
+            value: `Hi! My name is Ghelonico G. Maligaya living in Antipolo City Rizal, Philippines. ${age} years old and
+        I'm currently employed at Monheim Group of Companies as a Programmer.`},
+        {
+            value: `With nearly ${exp} years of experience, I have honed my programming skills, specializing in the development of web,
+            mobile, and desktop applications. My expertise extends to navigating agile software development processes and workflows.`
+        }
+    ];
+
+    const cards = [
+        { id: `summ`, value: `Summary`, component: Summary },
+        { id: `educ`, value: `Education` },
+        { id: `exp`, value: `Experience` },
+        { id: `cont`, value: `Contact` },
+    ]
+
+    const renderComponent = () => {
+        const selectedCard = cards.find((card) => card.id === cardType);
+        if (selectedCard && selectedCard.component){
+            const CardComponent = selectedCard.component;
+            return <CardComponent />;
+        }
+
+        return null;
+    }
 
     return (
-        <div className='about-page'>
-            <div className="abt-propic">
-                <img src={propic} alt='profile-pic' />
-                <h5>" Ghelo "</h5>
-                <h6>Software Developer</h6>
-            </div>
-
-            <div className='abt-information'>
-                <p>
-                    Hi! My name is Ghelonico G. Maligaya living in Antipolo City Rizal, Philippines. {age} years old and
-                    I'm currently employed at Monheim Group of Companies as a Programmer.
-                </p>
-
-                <p>
-                    With nearly {exp} years of experience, I have honed my programming skills, specializing in the development of web,
-                    mobile, and desktop applications. My expertise extends to navigating agile software development processes and workflows.
-                </p>
-
-                <h3>Summary</h3>
-
-                <div className='abt-summary'>
-                    <h6>Age : </h6>
-                    <h5> {age}</h5>
+        <div>
+            {cardType ? (
+                <div>
+                    {renderComponent()}
                 </div>
-                <div className='abt-summary'>
-                    <h6>Age : </h6>
-                    <h5> {age}</h5>
+            ) : (
+                <div className='about-page'>
+                    <div className="abt-propic">
+                        <img src={propic} alt='profile-pic' />
+                        <h5>Ghelo</h5>
+                        <h6>Software Developer</h6>
+                    </div>
+
+                    <div className='abt-information'>
+                        {paragraphItems.map((item, index) => (
+                            <p key={index}>
+                                {item.value}
+                            </p>
+                        ))}
+
+                        <div className='abt-cards'>
+                            {cards.map((item, index) => (
+                                <div className='abt-card' key={index} onClick={() => setCardType(item.id)}>
+                                    <h3>{item.value}</h3>
+                                </div>
+                            ))}
+                        </div>
+                    </div>
                 </div>
-            </div>
+            )}
 
         </div>
     )
---
+++ src/components/about/Summary.jsx
@@ -0,0 +1,19 @@
+import '../../css/about/summary.css'
+
+const Summary = () => {
+    return (
+        <div className='summ-page'>
+            <h2>SUMMARY</h2>
+            <h2>SUMMARY</h2>
+            <h2>SUMMARY</h2>
+            <h2>SUMMARY</h2>
+            <h2>SUMMARY</h2>
+            <h2>SUMMARY</h2>
+            <h2>SUMMARY</h2>
+            <h2>SUMMARY</h2>
+            <a href='/about'>BALIK</a>
+        </div>
+    )
+} 
+
+export default Summary
